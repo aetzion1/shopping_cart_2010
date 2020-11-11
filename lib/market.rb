05ae6835ebc8325ca7a8e7789965e1f3de3b57b3
Adam Etzion <adametzion90@gmail.com>
2020-11-11 10:32:27 -0700

    Add total_inventory Market method

--- lib/market.rb
+++ lib/market.rb
@@ -24,16 +24,41 @@ class Market
     end
 
     def sorted_item_list
-        
-        list = []
+        list = @vendors.map do |vendor|
+            vendor.inventory.map do |item|
+                item[0].name if item[1] > 0
+            end
+        end.flatten
+
+        list.sort.uniq
+    end
+
+    def total_inventory
+        items = @vendors.map do |vendor|
+            vendor.inventory.map do |item|
+                item[0] if item[1] > 0
+            end
+        end.flatten.uniq
+
+        total_inventory = {}
+
+        items.each do |item|
+            total_inventory[item] = {
+                quantity: 0,
+                vendors: []
+            }
+        end
+
         @vendors.each do |vendor|
-            vendor.inventory.each do |item|
-                if item[1] > 0
-                    list << item[0].name
-                end
+            total_inventory.each do |item|
+                item[1][:quantity] += vendor.check_stock(item[0])
             end
         end
-        list.sort.uniq
+
+        total_inventory.each do |item|
+            item[1][:vendors] = vendors_that_sell(item[0])
+        end
+        total_inventory
     end
 
 end
