7260e5b4a16255b7f06af477c77c3c29726454f9
Adam Etzion <adametzion90@gmail.com>
2020-11-11 09:20:29 -0700

    Add vendor initialize, check_stock, stock methods

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,8 +1,9 @@
 require 'Minitest/autorun'
 require 'Minitest/pride'
-require './lib/'
+require './lib/vendor'
+require './lib/item'
 
-class _CLASS < Minitest::Test
+class VendorTest < Minitest::Test
     def setup
         @vendor = Vendor.new("Rocky Mountain Fresh")
         @item1 = Item.new({name: 'Peach', price: "$0.75"})
@@ -11,21 +12,21 @@ class _CLASS < Minitest::Test
     
     def test_it_exists_and_has_attributes
         assert_instance_of Vendor, @vendor
-        assert_equals "Rocky Mountain Fresh", @vendor.name
-        assert_equals ({}), @vendor.inventory
+        assert_equal "Rocky Mountain Fresh", @vendor.name
+        assert_equal ({}), @vendor.inventory
     end
 
     def test_check_stock
-        assert_equal 0, @vendor,check_stock(@item1)
+        assert_equal 0, @vendor.check_stock(@item1)
     end
 
     def test_stock
         @vendor.stock(@item1, 30)
         assert_equal ({@item1 => 30}), @vendor.inventory
-        assert_equal 30, @vendor,check_stock(@item1)
+        assert_equal 30, @vendor.check_stock(@item1)
 
         @vendor.stock(@item1, 25)
-        assert_equal 55, @vendor,check_stock(@item1)
+        assert_equal 55, @vendor.check_stock(@item1)
         
         @vendor.stock(@item2, 12)
         assert_equal ({@item1 => 55, @item2 => 12}), @vendor.inventory
